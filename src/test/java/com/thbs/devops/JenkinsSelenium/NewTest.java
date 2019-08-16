469eb7eff819e9c3c95b0b5eafb508c7d5099bf8
devopss2019 <53856355+devopss2019@users.noreply.github.com>
2019-08-16 15:17:11 +0530

    Update NewTest.java

--- src/test/java/com/thbs/devops/JenkinsSelenium/NewTest.java
+++ src/test/java/com/thbs/devops/JenkinsSelenium/NewTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
+import java.lang.System;
 
 public class NewTest {
 	WebDriver driver;
