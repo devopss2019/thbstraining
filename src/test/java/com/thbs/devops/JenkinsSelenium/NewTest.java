a27d7057938b1758a161a66fda61cc95d7e766fb
devopss2019 <53856355+devopss2019@users.noreply.github.com>
2019-08-16 15:11:29 +0530

    Update NewTest.java

--- src/test/java/com/thbs/devops/JenkinsSelenium/NewTest.java
+++ src/test/java/com/thbs/devops/JenkinsSelenium/NewTest.java
@@ -4,23 +4,41 @@ import org.testng.annotations.Test;
 
 
 import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
+	WebDriver driver;
+	 
+	 
+ @BeforeTest
+ public  void launshSite() {
+	 System.setProperty("webdriver.chrome.driver", "D:/CoE/BootcampTraining/Test/chromedriver_win32/chromedriver.exe");
+	  driver = new org.openqa.selenium.chrome.ChromeDriver();
+	 driver.get("http://www.google.co.in");
+ }
+   
   @Test
-  public void f() {
-	  System.setProperty("webdriver.chrome.driver", "D:/CoE/BootcampTraining/Test/chromedriver_win32/chromedriver.exe");
-      org.openqa.selenium.WebDriver driver = new org.openqa.selenium.chrome.ChromeDriver();
-      driver.get("http://www.google.co.in");
-      System.out.println("Selenium Webdriver Script in Chrome browser | Software Testing Material");
-      driver.close();
+  public void enterText() {
+	  WebElement e1 = driver.findElement(By.name("q"));
+      e1.sendKeys("Selenium");
+      e1.submit();
   }
-  @BeforeTest
-  public void beforeTest() {
+      
+  @Test
+  public void waitFor() {
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+     
   }
-
+  
   @AfterTest
   public void afterTest() {
+	  driver.close();
   }
 
 }
